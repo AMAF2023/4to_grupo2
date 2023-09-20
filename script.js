5cca704e95a2d8bb8fbaab3f76717d4e90adbae4
AMAF <github_amaf@outlook.com>
2023-09-20 08:24:17 -0300

    modificaciones

--- script.js
+++ script.js
@@ -5,7 +5,7 @@ function scrollFunction() {
     var winScroll = document.body.scrollTop || document.documentElement.scrollTop;
     var height = document.documentElement.scrollHeight - document.documentElement.clientHeight;
     var scrolled = (winScroll / height) * 100;
-    document.getElementById("myBar").style.width = scrolled + "%";
+    document.getElementById("miBarra").style.width = scrolled + "%";
 }
 
 
